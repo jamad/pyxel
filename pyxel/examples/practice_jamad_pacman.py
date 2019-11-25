95ea28ac77d8db7f3d8f24f836af2bf6ef691ed4
jamad <jamad2001@gmail.com>
2019-11-25 23:14:23 +0200

    Update practice_jamad_pacman.py
    clean up + pacman clears dots

--- pyxel/examples/practice_jamad_pacman.py
+++ pyxel/examples/practice_jamad_pacman.py
@@ -3,10 +3,9 @@ import pyxel
 
 class Pacman:#pacman自体を生成するクラス
     def __init__(self):
-        self.P=[8,8]
+        self.P=[8,8]# unit: pixel
         self.V=[0,0]# パックマンの進む方向の情報 unit: タイル
-        self.T=[1,1]# 今いるタイルを座標化する
-        self.ori=0# 0方向なし, 1 上, 2を右,3を下,4を左
+        self.T=[1,1]# 今いるタイルを座標化する unit: タイル
         self.pattern = 0# どのパックマンをプロットするかの情報
         self.score = 0# スコア
 
@@ -14,9 +13,8 @@ class App:
     def __init__(self):
         init(128,140,caption="pacman-ish", fps=25,scale=2)
         load('pacman.pyxres')
-        self.pacman=Pacman() #################  抜けてるだろ！
-        self.tilemap_state=tilemap(0)#################  抜けてるだろ！ 超重要！！！
-
+        self.pacman=Pacman() #################  抜けてた！
+        self.tilemap_state=tilemap(0)#################  抜けてた！ 超重要！！！　偶然でも、よく見つけたな俺
         run(self.update, self.draw)# 毎フレーム毎にupdateとdrawを呼び出す
 
     def update(self):# ゲーム内情報更新、キー入力処理
@@ -24,32 +22,36 @@ class App:
         if self.pacman.P[0]%8==0 and self.pacman.P[1]%8==0:# 移動判定
             if self.tilemap_state.get(self.pacman.T[0]+self.pacman.V[0],self.pacman.T[1]+self.pacman.V[1])==33:# 進行方向を確認 壁に行こうとしてたら
                 self.pacman.V[0] = self.pacman.V[1] = 0# stop
-            elif btn(KEY_W) and self.tilemap_state.get(self.pacman.T[0],self.pacman.T[1]-1)!=33:self.pacman.V= [0,-1] # 上に行くように設定
-            elif btn(KEY_S) and self.tilemap_state.get(self.pacman.T[0],self.pacman.T[1]+1)!=33:self.pacman.V =[0,1] # 描画する向きを下に設定
-            elif btn(KEY_D) and self.tilemap_state.get(self.pacman.T[0]+1,self.pacman.T[1])!=33:self.pacman.V= [1,0]# 描画する向きを右に設定
-            elif btn(KEY_A) and self.tilemap_state.get(self.pacman.T[0]-1,self.pacman.T[1])!=33:self.pacman.V =[-1,0]# 描画する向きを左に設定
+            elif btn(KEY_W) and self.tilemap_state.get(self.pacman.T[0],self.pacman.T[1]-1)!=33:self.pacman.V=[0,-1]# 上に行くように設定
+            elif btn(KEY_S) and self.tilemap_state.get(self.pacman.T[0],self.pacman.T[1]+1)!=33:self.pacman.V=[0,1] # 描画する向きを下に設定
+            elif btn(KEY_D) and self.tilemap_state.get(self.pacman.T[0]+1,self.pacman.T[1])!=33:self.pacman.V=[1,0] # 描画する向きを右に設定
+            elif btn(KEY_A) and self.tilemap_state.get(self.pacman.T[0]-1,self.pacman.T[1])!=33:self.pacman.V=[-1,0]# 描画する向きを左に設定
 
             self.pacman.T=[x+y for x,y in zip(self.pacman.T,self.pacman.V)]# タイルの座標を進行方向に合わせて変える
 
         self.pacman.P=[x+y for x,y in zip(self.pacman.P,self.pacman.V)]# 毎フレーム毎に描画する座標を変更していく
 
-        # update tile 
-        if self.tilemap_state.get(self.pacman.T[0],self.pacman.T[1]) == 64:self.pacman.score += 30# ノーマルクッキーに触れたら
-        elif self.tilemap_state.get(self.pacman.T[0],self.pacman.T[1]) == 65:self.pacman.score += 100# パワークッキーに触れたら
-        pyxel.tilemap(0).set(self.pacman.T[0],self.pacman.T[1],5)# パックマンがノーマルクッキーとパワークッキーを踏んだら、何もないブロックに変える
+        # clear tile 
+        current_tile=self.tilemap_state.get(self.pacman.T[0],self.pacman.T[1])
+        if current_tile==64:self.pacman.score += 10# ノーマルクッキーに触れたら
+        if current_tile==65:self.pacman.score += 50# パワークッキーに触れたら
+        pyxel.tilemap(0).set(self.pacman.T[0],self.pacman.T[1],5)# パックマン後は、何もないブロックに変える
 
     def draw(self):# ゲーム内描画処理
         cls(0)# 真っ黒背景塗りつぶし
         
-        # タイルマップの描画処理
+        # タイルマップ描画
         x=y=u=v=tm=0#template番号
         w=h=16# block size
         bltm(x,y,tm,u,v,w,h)# 指定したtemplate番号のuv座標をxy座標にマップして描画する
 
-        # pacmanの描画処理
+        # pacman描画
         if self.pacman.V[0]  or self.pacman.V[1]:self.pacman.pattern={(0,-1):8,(0,1):24,(1,0):16,(-1,0):32}[tuple(self.pacman.V)]# 移動する場合、向きに応じた口パターンをゲット
         if pyxel.frame_count%2==0:self.pacman.pattern = 0# 口の動きを表現するために、2フレームに１回丸の状態を表示する
         # イメージ０に登録されている(self.pacman.pattern,0)の座標から 8×8サイズを参照にして、(self.pacman.P[0],self.pacman.P[1])の座標に描画する
         pyxel.blt(self.pacman.P[0],self.pacman.P[1],0,self.pacman.pattern,0,8,8)
 
+        #dscore描画
+        score = "SCORE : {:05}".format(self.pacman.score)
+        pyxel.text(1, 132, score, 6)
 App()
