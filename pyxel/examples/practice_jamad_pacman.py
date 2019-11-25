4d61b5e04745960f52c3b94b0632d1c7ab776b18
jamad <jamad2001@gmail.com>
2019-11-25 20:11:26 +0200

    Update practice_jamad_pacman.py

--- pyxel/examples/practice_jamad_pacman.py
+++ pyxel/examples/practice_jamad_pacman.py
@@ -1,39 +1,19 @@
-import pyxel
+from pyxel import init,load,run,btnp,KEY_Q,cls,bltm
 
 class App:
     def __init__(self):
-        # ゲームの設定。widthとheightの単位はドット
-        # init(width, height, [caption], [fps])
-        pyxel.init(128,128,caption="パックマンぽいゲーム", fps=25,scale=2)
+        init(128,128,caption="pacman-ish", fps=25,scale=2)
+        load('pacman.pyxres')
+        run(self.update, self.draw)# 毎フレーム毎にupdateとdrawを呼び出す
 
-        # 作成したドット絵やタイルマップの情報を読み込む
-        pyxel.load('pacman.pyxres')
-        # 毎フレーム毎にupdateとdrawを呼び出す
-        pyxel.run(self.update, self.draw)
+    def update(self):# ゲーム内情報更新、キー入力処理
+        if btnp(KEY_Q):quit()# qキーで終了
 
-    # ゲーム内で扱う情報を更新したり、キー入力の処理などを行う
-    def update(self):
-        # qキーが押されたらゲームを終了する。
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
+    def draw(self):# ゲーム内描画処理
+        cls(0)# 真っ黒背景塗りつぶし
 
-    # ゲーム内で描画されるドット絵の処理をする
-    def draw(self):
-        # 真っ黒に背景をする
-        pyxel.cls(0)
-        # タイルマップを描画する
-        self.tilemap_draw()
-
-    # タイルマップの描画処理
-    def tilemap_draw(self):
-        base_x = 0
-        base_y = 0
-        tm = 0
-        u = 0
-        v = 0
-        w = 16
-        h = 16
-        # 指定したtm(template)番号の(u,v)座標から
-        # サイズ(w,h)の大きさを(base_x,base_y)座標に描画する
-        pyxel.bltm(base_x,base_y,tm,u,v,w,h)
+        # タイルマップの描画処理
+        x=y=u=v=tm=0#template番号
+        w=h=16# block size
+        bltm(x,y,tm,u,v,w,h)# 指定したtemplate番号のuv座標をxy座標にマップして描画する
 App()
