eb1c703e67a077de1a44561220755767358f30ab
Junichi Yamada <junichi.yamada@ubisoft.com>
2019-11-27 17:31:11 +0200

    Update 08_triangle.py
    1 liner

--- pyxel/examples/08_triangle.py
+++ pyxel/examples/08_triangle.py
@@ -1,2 +1,1 @@
-import pyxel as P
-P.init(128,128,scale=1),P.run(lambda:None,lambda:[P.cls(5),P.tri(64,0,0,64,128,128,8)])
+import pyxel as P;P.init(128,128,scale=1),P.run(lambda:0,lambda:[P.cls(5),P.tri(64,0,0,64,128,128,8)])
