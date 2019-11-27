fa24dbef235f980c15f08771ba73dcd8b56c2ff6
Junichi Yamada <junichi.yamada@ubisoft.com>
2019-11-27 13:09:07 +0200

    Update 03_draw_api.py

--- pyxel/examples/03_draw_api.py
+++ pyxel/examples/03_draw_api.py
@@ -1,27 +1,27 @@
-import math
-import pyxel
+import pyxel,math
 from pyxel import init,image,tilemap,mouse,run,btn,cls,KEY_SPACE,btnp,KEY_Q,quit,text,clip,pix,line,rect,rectb,circ,circb,blt,bltm,pal
 class App:
     def __init__(self):
-        init(256, 256, caption="Pyxel Draw API",scale=2)
+        init(256, 256, caption="Pyxel Draw API",scale=1)
         image(0).load(0, 0, "assets/cat_16x16.png")
         image(1).load(0, 0, "assets/tileset_24x32.png")
         tilemap(0).set( 0,0, ["022000002004001000060061062000040", "042003020021022003000001002003060"],)
         tilemap(0).refimg = 1
-        self.pal_test_is_enabled = 0
-        self.clip_test = 0
+        self.pallet_test=0
+        self.clip_test =0
         mouse(1)
         run(self.update, self.draw)
     def update(self):
-        self.pal_test_is_enabled = (pyxel.frame_count // 30) % 10 >= 5
-        self.clip_test = btn(KEY_SPACE)
+        self.pallet_test=(pyxel.frame_count//20)%4
+        print(self.pallet_test)
+        self.clip_test=btn(KEY_SPACE)
         if btnp(KEY_Q):quit()
     def draw(self):
         fc=pyxel.frame_count
-        if self.pal_test_is_enabled:_,__,___=pal(2,3),pal(4,7),pal(7,10) # pallet swap - test_pal1
+        if self.pallet_test==1:pal(2,3)# pallet swap - test_pal1
+        if self.pallet_test==2:pal(4,7)
+        if self.pallet_test==3:pal(7,10)
         _,__=cls(2),text(6,6, "cls(col)",7) #self.test_cls(6, 6)
-        
-        self.test_clip()
         
         text(6, 20, "pix(x,y,col)", 7)
         for i in range(16):pix(10 + i*2, 30, i)
@@ -54,17 +54,14 @@ class App:
         text(106, 88, "bltm(x,y,tm,u,v,\n w,h,[colkey])", 7)
         bltm(106, 103, 0, 0, 0, 11, 2, 2)
 
-        text(6, 124, "text(x,y,s,col)", 7)
-        x,y=10,132
-        s = "Elapsed frame count is {}\n" "Current mouse position is ({},{})".format(fc, pyxel.mouse_x, pyxel.mouse_y    )
-        text(x+1,y,s,1)# shadow
-        text(x,y,s,9)
+        text(6, 124, "text(x,y,s,col)",7)
+        s = "Elapsed frame count is {}\n" "Current mouse position is ({},{})".format(fc,pyxel.mouse_x,pyxel.mouse_y)
+        text(11,133,s,1)# shadow
+        text(10,132,s,9)
 
         _,__=text(106, 124, "pal(col1,col2)", 4),pal()# test_pal2 
 
-    def test_clip(self):
         if not self.clip_test:return
-        fc=pyxel.frame_count
         clip()
         x,y,w,h=math.sin(fc*0.02)*39+40,math.sin(fc*0.03)*29+30,120,90
         text(x,y-8,"clip(x,y,w,h)",14)
