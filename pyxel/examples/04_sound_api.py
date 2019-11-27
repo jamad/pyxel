8c9cd342e7541c79398c88f2b87795fb80444629
Junichi Yamada <junichi.yamada@ubisoft.com>
2019-11-27 16:42:10 +0200

    Update 04_sound_api.py

--- pyxel/examples/04_sound_api.py
+++ pyxel/examples/04_sound_api.py
@@ -1,9 +1,8 @@
 import pyxel,math
-from pyxel import init,image,sound,run,play,stop,cls,btnp,KEY_Q,text,rect,rectb,pal,play_pos,blt,quit
-
+from pyxel import init,image,sound,run,play,stop,cls,btnp,text,rect,rectb,pal,play_pos,blt,quit
 class App:
     def __init__(self):
-        init(200, 150, caption="Pixel Sound API",scale=2)
+        init(200, 150, caption="Pixel Sound API",scale=1)
         image(0).set(0,0,[ "00011000", "00010100", "00010010","00010010","00010100","00010000","01110000","01100000"])
         sound(0).set( "e2e2c2g1 g1g1c2e2 d2d2d2g2 g2g2rr" "c2c2a1e1 e1e1a1c2 b1b1b1e2 e2e2rr","p","6","vffn fnff vffs vfnn",25)
         sound(1).set("r a1b1c2 b1b1c2d2 g2g2g2g2 c2c2d2e2" "f2f2f2e2 f2e2d2c2 d2d2d2d2 g2g2r r ","s","6","nnff vfff vvvv vfff svff vfff vvvv svnn",  25 )
@@ -50,7 +49,7 @@ class App:
         for i in range(3):
             x = 140 + i * 16
             y = 123 + math.sin(pyxel.frame_count * 0.1 + i * 2.1) * 5
-            col = 15 if pyxel.play_pos(i) >= 0 else 13
+            col = 15 if play_pos(i) >= 0 else 13
             pal(1, col)
             blt(x, y, 0, 0, 0, 8, 8, 0)
         pal()
