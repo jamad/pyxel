de1aa3d92fa030cf5e4bfe2bfffd95346f248da1
jamad <jamad2001@gmail.com>
2019-11-28 23:42:41 +0200

    Update 05_color_palette.py
    reduce more lines

--- pyxel/examples/05_color_palette.py
+++ pyxel/examples/05_color_palette.py
@@ -1,22 +1,20 @@
 from pyxel import init,run,btnp,quit,cls,DEFAULT_PALETTE,rect,text,rectb
-import pyxel
+import pyxel as P
 class App:
     def __init__(self):
         init(255, 81, caption="Pyxel Color Palette",scale=2)
         run(self.update, self.draw)
     def update(self):
-        if btnp(pyxel.KEY_Q):quit()
-        if btnp(pyxel.KEY_Z):pass # need to know to change scale from 1 to 2
+        if btnp(P.KEY_Q):quit()
+        if btnp(P.KEY_Z):pass # need to know to change scale from 1 to 2
     def draw(self):
         cls(0)
         for i in range(16):
-            x, y, col=(2 + (i % 4) * 64, 4 + (i // 4) * 20, i)
-            col_val = DEFAULT_PALETTE[col]
-            hex_col = "#{:06X}".format(col_val)
-            rgb_col = "{},{},{}".format( col_val >> 16, (col_val >> 8) & 0xFF, col_val & 0xFF)
-            rect(x, y, 13, 13, col)
-            text(x + 16, y + 1, hex_col, 7)
-            text(x + 16, y + 8, rgb_col, 7)
-            text(x + 5 - (col // 10) * 2, y + 4, "{}".format(col), 7 if col < 6 else 0      )
-            if col == 0:rectb(x, y, 13, 13, 5)
+            x, y, col=2+i%4*64, 4+i//4*20, i
+            v = DEFAULT_PALETTE[col]
+            if col:rect(x, y, 13, 13, col)
+            else:rectb(x, y, 13, 13, 5)
+            text(x+16, y+1, "#{:06X}".format(v), 7)
+            text(x+16, y+8, f"{v>>16},{(v>>8)&0xFF},{v&0xFF}",7)
+            text(x+5-col//10*2, y+4, f"{col}",7*(col<6))
 App()
